c4153c8cd862c3b0a17f71feb339c4156113b31a
Fabian Huegel <fabian.huegel@fau.de>
2018-09-16 13:40:46 +0200

    finish cbuf

--- cbuf.c
+++ cbuf.c
@@ -6,11 +6,48 @@
 int main(void)
 {
 	struct cbuf cbuf;
-	cbuf_init(&cbuf, malloc(16), 16);
-	const char *str = "abcdefghijklmnopqrstuvwxyz";
-	cbuf_write(&cbuf, 54, str, strlen(str));
-	char buf[44];
-	cbuf_read(&cbuf, 6, buf, sizeof(buf) - 1);
-	buf[sizeof(buf) - 1] = 0;
+	size_t n = 16;
+	cbuf_init(&cbuf, malloc(n), n);
+
+	const char *str = "ABCDEFGHIHKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+	n = cbuf_push(&cbuf, str, strlen(str), false);
+	printf("%lu\n", n);
+	n = cbuf_push(&cbuf, str, strlen(str), false);
+	printf("%lu\n", n);
+
+	char buf[128];
+	n = cbuf_pop(&cbuf, buf, 20);
+	printf("%lu\n", n);
+	buf[n] = 0;
 	puts(buf);
+	size_t k = cbuf_pop(&cbuf, buf + n, sizeof(buf) - 1 - n);
+	printf("%lu\n", k);
+	buf[n + k] = 0;
+	puts(buf);
+
+	cbuf_flush(&cbuf);
+
+	n = 0;
+	for (size_t i = 0; i < 80; i++) {
+		bool b = cbuf_pushb(&cbuf, '0' + i, true);
+		if (b) {
+			n++;
+		}
+	}
+
+	printf("%lu\n", n);
+	if (n > cbuf.capacity) {
+		n = cbuf.capacity;
+	}
+
+	for (size_t i = 0; i < n; i++) {
+		char c;
+		bool b = cbuf_popb(&cbuf, &c);
+		assert(b);
+		putchar(c);
+	}
+	putchar('\n');
+
+	free(cbuf.buf);
+	return 0;
 }
--- cbuf.h
+++ cbuf.h
@@ -2,34 +2,106 @@
 #define __cbuf_include__
 
 #include <string.h>
+#include <stdbool.h>
+#include <assert.h>
 
 struct cbuf {
 	char *buf;
-	size_t size;
+	size_t capacity;
 	size_t start;
 	size_t end;
 };
 
-static void cbuf_init(struct cbuf *cbuf, void *mem, size_t size)
+static void cbuf_init(struct cbuf *cbuf, void *mem, size_t capacity)
 {
-	assert(size != 0 && (size & (size - 1)) == 0);
+	assert(capacity != 0 && (capacity & (capacity - 1)) == 0);
 
 	cbuf->buf = mem;
-	cbuf->size = size;
+	cbuf->capacity = capacity;
 	cbuf->start = 0;
 	cbuf->end = 0;
 }
 
+static inline void cbuf_flush(struct cbuf *cbuf)
+{
+	cbuf->start = cbuf->end;
+}
+
+static inline size_t cbuf_size(struct cbuf *cbuf)
+{
+	return cbuf->end - cbuf->start;
+}
+
+static inline size_t cbuf_avail_size(struct cbuf *cbuf)
+{
+	return cbuf->capacity - cbuf_size(cbuf);
+}
+
+static bool cbuf_pushb(struct cbuf *cbuf, char byte, bool overwrite)
+{
+	size_t avail = cbuf_avail_size(cbuf);
+	if (!overwrite && avail == 0) {
+		return false;
+	}
+	size_t index = cbuf->end & (cbuf->capacity - 1);
+	cbuf->buf[index] = byte;
+	cbuf->end++;
+	if (cbuf_size(cbuf) > cbuf->capacity) {
+		cbuf->start = cbuf->end - cbuf->capacity;
+	}
+	return true;
+}
+
+static bool cbuf_popb(struct cbuf *cbuf, char *bytep)
+{
+	size_t avail = cbuf_size(cbuf);
+	if (avail == 0) {
+		return false;
+	}
+	size_t index = cbuf->start & (cbuf->capacity - 1);
+	*bytep = cbuf->buf[index];
+	cbuf->start++;
+	return true;
+}
+
+static void cbuf_write(struct cbuf *cbuf, size_t offset, const void *_buf, size_t count);
+static void cbuf_read(struct cbuf *cbuf, size_t offset, void *_buf, size_t count);
+
+static size_t cbuf_push(struct cbuf *cbuf, const void *buf, size_t count, bool overwrite)
+{
+	size_t avail = cbuf_avail_size(cbuf);
+	if (!overwrite && avail < count) {
+		count = avail;
+	}
+	cbuf_write(cbuf, cbuf->end, buf, count);
+	cbuf->end += count;
+	if (cbuf_size(cbuf) > cbuf->capacity) {
+		cbuf->start = cbuf->end - cbuf->capacity;
+	}
+	return count;
+}
+
+static size_t cbuf_pop(struct cbuf *cbuf, void *buf, size_t count)
+{
+	size_t avail = cbuf_size(cbuf);
+	if (avail < count) {
+		count = avail;
+	}
+	cbuf_read(cbuf, cbuf->start, buf, count);
+	cbuf->start += count;
+	return count;
+}
+
 static void cbuf_write(struct cbuf *cbuf, size_t offset, const void *_buf, size_t count)
 {
-	size_t skip = count > cbuf->size ? count - cbuf->size : 0;
+	size_t skip = count > cbuf->capacity ? count - cbuf->capacity : 0;
 	const char *buf = (const char *)_buf + skip;
 	offset += skip;
 	count -= skip;
-	offset &= cbuf->size - 1;
+	offset &= cbuf->capacity - 1;
 	char *start = cbuf->buf + offset;
-	if (offset + count > cbuf->size) {
-		size_t n = cbuf->size - offset;
+	if (offset + count > cbuf->capacity) {
+		size_t n = cbuf->capacity - offset;
 		memcpy(start, buf, n);
 		count -= n;
 		buf += n;
@@ -41,19 +113,19 @@ static void cbuf_write(struct cbuf *cbuf, size_t offset, const void *_buf, size_
 static void cbuf_read(struct cbuf *cbuf, size_t offset, void *_buf, size_t count)
 {
 	char *buf = (char *)_buf;
-	offset &= cbuf->size - 1;
-	size_t n = cbuf->size - offset;
-	if (n > count) {
+	offset &= cbuf->capacity - 1;
+	size_t n = cbuf->capacity - offset;
+	if (n >= count) {
 		memcpy(buf, cbuf->buf + offset, count);
 		return;
 	}
 	memcpy(buf, cbuf->buf + offset, n);
 	buf += n;
 	count -= n;
-	while (count > cbuf->size) {
-		memcpy(buf, cbuf->buf, cbuf->size);
-		buf += cbuf->size;
-		count -= cbuf->size;
+	while (count > cbuf->capacity) {
+		memcpy(buf, cbuf->buf, cbuf->capacity);
+		buf += cbuf->capacity;
+		count -= cbuf->capacity;
 	}
 	memcpy(buf, cbuf->buf, count);
 }
