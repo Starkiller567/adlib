fd5c0b56f7cccb8c33a2eefa060c8149a0b8574f
Fabian Huegel <fabian.huegel@fau.de>
2018-09-17 16:57:17 +0200

    endianess stuff

---
+++ endian.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include "endian.h"
+
+int main(void)
+{
+#define x 0xdeadbeefcafebabe
+
+	uint16_t x16 = (uint16_t)x;
+	uint32_t x32 = (uint32_t)x;
+	uint64_t x64 = (uint64_t)x;
+	__be16 be16 = cpu_to_be16(x16);
+	__be32 be32 = cpu_to_be32(x32);
+	__be64 be64 = cpu_to_be64(x64);
+	__le16 le16 = cpu_to_le16(x16);
+	__le32 le32 = cpu_to_le32(x32);
+	__le64 le64 = cpu_to_le64(x64);
+
+
+	printf("le: %i, be: %i\n", is_little_endian(), is_big_endian());
+	puts("");
+	printf("%x\n", x16);
+	printf("%x\n", be16);
+	printf("%x\n", le16);
+	printf("%x\n", be16_to_cpu(be16));
+	printf("%x\n", le16_to_cpu(le16));
+	puts("");
+	printf("%x\n", x32);
+	printf("%x\n", be32);
+	printf("%x\n", le32);
+	printf("%x\n", be32_to_cpu(be32));
+	printf("%x\n", le32_to_cpu(le32));
+	puts("");
+	printf("%lx\n", x64);
+	printf("%lx\n", be64);
+	printf("%lx\n", le64);
+	printf("%lx\n", be64_to_cpu(be64));
+	printf("%lx\n", le64_to_cpu(le64));
+}
---
+++ endian.h
@@ -0,0 +1,111 @@
+#ifndef __endian_include__
+#define __endian_include__
+
+#include <stdbool.h>
+#include <stdint.h>
+
+#define bswap64(x) ((((uint64_t)x & 0xff00000000000000) >> 56) |	\
+                    (((uint64_t)x & 0x00ff000000000000) >> 40) |	\
+                    (((uint64_t)x & 0x0000ff0000000000) >> 24) |	\
+                    (((uint64_t)x & 0x000000ff00000000) >>  8) |	\
+                    (((uint64_t)x & 0x00000000ff000000) <<  8) |	\
+                    (((uint64_t)x & 0x0000000000ff0000) << 24) |	\
+                    (((uint64_t)x & 0x000000000000ff00) << 40) |	\
+                    (((uint64_t)x & 0x00000000000000ff) << 56))
+
+#define bswap32(x) ((((uint32_t)x & 0xff000000) >> 24) |	\
+                    (((uint32_t)x & 0x00ff0000) >>  8) |	\
+                    (((uint32_t)x & 0x0000ff00) <<  8) |	\
+                    (((uint32_t)x & 0x000000ff) << 24))
+
+#define bswap16(x) ((((uint16_t)x & 0xff00) >> 8) |	\
+                    (((uint16_t)x & 0x00ff) << 8))
+
+
+static inline bool is_little_endian()
+{
+	const union {
+		int i;
+		char c;
+	} u = {1};
+	return u.c == 1;
+}
+
+static inline bool is_big_endian()
+{
+	const union {
+		int i;
+		char c;
+	} u = {1};
+	return u.c == 0;
+}
+
+typedef uint16_t __be16;
+typedef uint32_t __be32;
+typedef uint64_t __be64;
+
+typedef uint16_t __le16;
+typedef uint32_t __le32;
+typedef uint64_t __le64;
+
+static inline __be16 cpu_to_be16(uint16_t u)
+{
+	return is_big_endian() ? u : bswap16(u);
+}
+
+static inline __be32 cpu_to_be32(uint32_t u)
+{
+	return is_big_endian() ? u : bswap32(u);
+}
+
+static inline __be64 cpu_to_be64(uint64_t u)
+{
+	return is_big_endian() ? u : bswap64(u);
+}
+
+static inline uint16_t be16_to_cpu(__be16 b)
+{
+	return is_big_endian() ? b : bswap16(b);
+}
+
+static inline uint32_t be32_to_cpu(__be32 b)
+{
+	return is_big_endian() ? b : bswap32(b);
+}
+
+static inline uint64_t be64_to_cpu(__be64 b)
+{
+	return is_big_endian() ? b : bswap64(b);
+}
+
+static inline __le16 cpu_to_le16(uint16_t u)
+{
+	return is_little_endian() ? u : bswap16(u);
+}
+
+static inline __le32 cpu_to_le32(uint32_t u)
+{
+	return is_little_endian() ? u : bswap32(u);
+}
+
+static inline __le64 cpu_to_le64(uint64_t u)
+{
+	return is_little_endian() ? u : bswap64(u);
+}
+
+static inline uint16_t le16_to_cpu(__le16 l)
+{
+	return is_little_endian() ? l : bswap16(l);
+}
+
+static inline uint32_t le32_to_cpu(__le32 l)
+{
+	return is_little_endian() ? l : bswap32(l);
+}
+
+static inline uint64_t le64_to_cpu(__le64 l)
+{
+	return is_little_endian() ? l : bswap64(l);
+}
+
+#endif
