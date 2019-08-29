cdc416b6dff5ff9ac6acb04790eb9632807db6be
Fabian Huegel <fabian.huegel@fau.de>
2019-08-29 16:38:21 +0200

    add more array functions

--- array.c
+++ array.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <stdarg.h>
+#include <unistd.h>
 #include <assert.h>
 #include "array.h"
 
 static void print_array(int *arr, bool print_reverse)
 {
 	size_t len = array_len(arr);
-	size_t limit = array_limit(arr);
-	printf("{\nlen = %zu\nlimit = %zu\nval = {", len, limit);
+	size_t limit = array_capacity(arr);
+	printf("{\n\tlen = %zu,\n\tlimit = %zu,\n\tval = {", len, limit);
 	int *cur;
 	size_t i = 0;
 	array_foreach(arr, cur) {
@@ -19,7 +21,7 @@ static void print_array(int *arr, bool print_reverse)
 	}
 
 	if (print_reverse) {
-		printf("}\nreverse = {");
+		printf("},\n\treverse = {");
 		array_foreach_reverse(arr, cur) {
 			printf("%i", *cur);
 			i--;
@@ -27,9 +29,26 @@ static void print_array(int *arr, bool print_reverse)
 				printf(", ");
 			}
 		}
+		assert(i == 0);
 	}
 
 	printf("}\n}\n");
+}
+
+static void assert_array_content(int *arr, ...)
+{
+	va_list args;
+	va_start(args, arr);
+	int *cur;
+	array_foreach(arr, cur) {
+		assert(*cur == va_arg(args, int));
+	}
+	va_end(args);
+}
+
+static int cmp(const void *a, const void *b)
+{
+	return *(const int *)a - *(const int *)b;
 }
 
 int main(void)
@@ -58,6 +77,9 @@ int main(void)
 	array_insert(arr1, 5, 5);
 
 	int *arr2 = array_copy(arr1);
+	print_array(arr1, true);
+	assert_array_content(arr2, 0, 1, 2, 3, 4, 5, 0, 1, 2, 3, 4, 5);
+
 
 	int i;
 	i = array_pop(arr1);
@@ -66,7 +88,7 @@ int main(void)
 	i = array_pop(arr1);
 	i = array_pop(arr1);
 	i = array_pop(arr1);
-	printf("%d\n", i);
+	assert(i == 0);
 
 	array_make_valid(arr1, 1);
 	array_make_valid(arr1, 7);
@@ -80,6 +102,7 @@ int main(void)
 	array_shrink_to_fit(arr2);
 
 	print_array(arr1, true);
+	assert_array_content(arr1, 0, 1, 2, 3, 4, 5, 6, 7);
 	array_ordered_deleten(arr1, 2, 1);
 	array_fast_deleten(arr1, 2, 1);
 
@@ -93,11 +116,14 @@ int main(void)
 
 	array_free(arr1);
 	array_insertn(arr1, 0, 10);
+	assert(array_capacity(arr1) >= 10);
 	array_free(arr1);
 
 	array_reserve(arr1, 1);
+	assert(array_capacity(arr1) >= 1);
 	print_array(arr1, true);
 	array_reserve(arr1, 5);
+	assert(array_capacity(arr1) >= 5);
 	print_array(arr1, true);
 	array_add(arr1, 0);
 	array_add(arr1, 0);
@@ -105,8 +131,62 @@ int main(void)
 	array_add(arr1, 0);
 	array_add(arr1, 0);
 	print_array(arr1, true);
+	assert_array_content(arr1, 0, 0, 0, 0, 0);
 	array_free(arr1);
 
+	int digits[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+	array_add_arrayn(arr1, digits, sizeof(digits) / sizeof(digits[0]));
+	arr2 = array_copy(arr1);
+	array_add_array(arr1, arr2);
+	print_array(arr1, true);
+	assert_array_content(arr1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9);
+	array_truncate(arr1, 10);
+	assert_array_content(arr1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9);
+	array_free(arr1);
+
+	array_reverse(arr2);
+	print_array(arr2, true);
+	assert_array_content(arr2, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0);
+
+	array_shuffle(arr2);
+	print_array(arr2, true);
+	array_shuffle(arr2);
+	print_array(arr2, true);
+	array_sort(arr2, cmp);
+	print_array(arr2, true);
+	assert_array_content(arr2, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9);
+	array_free(arr2);
+
+	array_add_arrayn(arr1, digits, 10);
+	int *dest = array_addn(arr2, 10);
+	memcpy(dest, digits, 10 * sizeof(int));
+	bool equal = array_equal(arr1, arr2);
+	assert(equal);
+	arr2[0] = 1;
+	equal = array_equal(arr1, arr2);
+	assert(!equal);
+	array_ordered_delete(arr2, 0);
+	equal = array_equal(arr1, arr2);
+	assert(!equal);
+	array_insert(arr2, 0, 0);
+	equal = array_equal(arr1, arr2);
+	assert(equal);
+	array_free(arr1);
+	array_free(arr2);
+
+	array_add_arrayn(arr1, digits, sizeof(digits) / sizeof(digits[0]));
+	for (size_t i = 0; i < 10; i++) {
+		array_shuffle(arr1);
+		int *cur;
+		array_foreach(arr1, cur) {
+			printf("%i ", *cur);
+		}
+		putchar('\r');
+		fflush(stdout);
+		usleep(300000);
+	}
+	putchar('\n');
+	array_free(arr1);
 #else
 
 	int n = 300000;
--- array.h
+++ array.h
@@ -6,6 +6,7 @@
 #include <stdlib.h>
 #include <string.h>
 
+// TODO also add safety guards to the end of the array
 #ifndef ARRAY_SAFETY_CHECKS
 # ifdef NDEBUG
 #  define ARRAY_SAFETY_CHECKS 0
@@ -21,7 +22,7 @@
 
 typedef struct {
 	size_t len;
-	size_t limit;
+	size_t capacity;
 #if ARRAY_SAFETY_CHECKS
 	size_t magic1;
 	size_t magic2;
@@ -38,46 +39,109 @@ typedef struct {
 # define __arrhead(a) ((__arr *)(a) - 1)
 #endif
 
-#define array_len(a)                   ((a) ? __arrhead(a)->len : 0)
-#define array_empty(a)                 (array_len(a) == 0)
-#define array_lasti(a)                 (array_len(a) - 1)
-#define array_last(a)                  ((a)[array_lasti(a)])
-#define array_limit(a)                 ((a) ? __arrhead(a)->limit : 0)
+// get number of elements in array
+#define array_len(a)                    ((a) ? __arrhead(a)->len : 0)
+// is array empty?
+#define array_empty(a)                  (array_len(a) == 0)
+// get last valid index
+#define array_lasti(a)                  (array_len(a) - 1)
+// get last element
+#define array_last(a)                   ((a)[array_lasti(a)])
+// get allocated capacity in elements
+#define array_capacity(a)               ((a) ? __arrhead(a)->capacity : 0)
+// release all resources of the array (will be set to NULL)
 #define array_free(a)                   __array_free((void **)&(a))
+// make an exact copy of the array
 #define array_copy(a)	                __array_copy((a), sizeof((a)[0]))
-#define array_addn(a, n)                __array_addn((void **)&(a), sizeof((a)[0]), n)
-#define array_add(a, v)                 (array_addn(a, 1), ((a)[array_lasti(a)]  = v))
+// add n unitialized elements to the end of the array and return a (void) pointer to the first of those
+// TODO figure out how to return a pointer of the original type
+#define array_addn(a, n)                (__array_addn((void **)&(a), sizeof((a)[0]), n))
+// add the element v to the end of the array
+#define array_add(a, v)                 (array_addn(a, 1), ((a)[array_lasti(a)] = v))
+// insert n unitialized elements at index i
 #define array_insertn(a, i, n)          __array_insertn((void **)&(a), sizeof((a)[0]), i, n)
+// insert element v at index i
 #define array_insert(a, i, v)           (array_insertn(a, i, 1), ((a)[i] = v))
+// make the array empty (but keep the memory)
 #define array_reset(a)	                do { if (a) __arrhead(a)->len = 0; } while(0);
-#define array_resize(a, limit)          __array_resize((void **)&(a), sizeof((a)[0]), limit)
+// set the capacity of the array
+#define array_resize(a, capacity)       __array_resize((void **)&(a), sizeof((a)[0]), capacity)
+// reduce the length of the array (noop if newlen >= len)
+#define array_truncate(a, newlen)       (newlen < array_len(a) ? (void)(__arrhead(a)->len = newlen) : (void)0)
+// allocate enough space for n additional elements (does not change length, only capacity)
 #define array_reserve(a, n)             __array_reserve((void **)&(a), sizeof((a)[0]), n)
+// increase capacity by atleast n elements
 #define array_grow(a, n)                __array_grow((void **)&(a), sizeof((a)[0]), n)
+// make capacity equal to length
 #define array_shrink_to_fit(a)          array_resize(a, array_len(a));
+// make sure the index i is within the allocated space
 #define array_make_valid(a, i)          __array_make_valid((void **)&(a), sizeof((a)[0]), i);
+// alias for add
 #define array_push(a, v)                array_add(a, v)
+// remove and return the last element
 #define array_pop(a)                    ((a)[--__arrhead(a)->len])
+// remove the last n elements (returns the new length)
 #define array_popn(a, n)	        (__arrhead(a)->len -= n)
+// delete n elements starting at index i WITHOUT keeping the original order of elements
 #define array_fast_deleten(a, i, n)     __array_fast_deleten((a), sizeof((a)[0]), i, n)
+// delete the element at index i WITHOUT keeping the original order of elements
 #define array_fast_delete(a, i)         (((a)[i] = array_last(a)), __arrhead(a)->len--)
+// delete n elements starting at index i keeping the original order of elements
 #define array_ordered_deleten(a, i, n)  __array_ordered_deleten((a), sizeof((a)[0]), i, n)
+// delete the element at index i keeping the original order of elements
 #define array_ordered_delete(a, i)      array_ordered_deleten(a, i, 1)
+// get the index of the element pointed to by ptr
 #define array_index_of(a, ptr)          ((size_t)((ptr) - (a)))
+// iterate over all array indices (the variable 'itername' contains the current index)
 #define array_fori(a, itername)         for (size_t itername = 0; itername < array_len(a); itername++)
+// iterate over all array indices in reverse (the variable 'itername' contains the current index)
 #define array_fori_reverse(a, itername) for (size_t itername = array_len(a); \
 					     !array_empty(a) && --itername != 0;)
+// iterate over all array elements (v must be a variable of type 'pointer to element' and contains the current element)
 #define array_foreach(a, v)             for ((v) = (a); (v) < (a) + array_len(a); (v)++)
+// iterate over all array elements in reverse (v must be a variable of type 'pointer to element' and contains the current element)
 #define array_foreach_reverse(a, v)     for ((v) = (a) + array_len(a);	\
 					     !array_empty(a) && --(v) >= (a);)
+// add the n first elements of array b to a (a and b should have the same type, but b can be a static array)
+#define array_add_arrayn(a, b, n)       (memcpy(array_addn(a, n), 1 ? (b) : (a), \
+						(n) * sizeof((a)[0])))
+// add all elements of (dynamic) array b to a (a and b should have the same type)
+#define array_add_array(a, b)           array_add_arrayn(a, b, array_len(b))
+// sort array using comparison function (see qsort documentation)
+#define array_sort(a, compare_fn)       qsort((a), array_len(a), sizeof((a)[0]), (compare_fn))
+// are arrays a and b equal in content (byte-wise equality)
+#define array_equal(a, b)               (array_len(a) != array_len(b) ? 0 : \
+					 memcmp(1 ? (a) : (b), (b), array_len(a) * sizeof((a)[0])) == 0)
+// swap elements at index idx1 and idx2
+#define array_swap(a, idx1, idx2)					\
+	do {								\
+		if (idx1 != idx2) {					\
+			unsigned char buf[sizeof((a)[0])];		\
+			__array_swap((a), sizeof((a)[0]), buf, (idx1), (idx2));	\
+		}							\
+	} while (0)
+// reverse the order of elements in the array
+#define array_reverse(a)					\
+	do {							\
+		for (size_t i = 0; i < array_len(a) / 2; i++) {	\
+			array_swap(a, i, array_lasti(a) - i);	\
+		}						\
+	} while (0)
+// shuffle the elements in the array (creates a random permutation of the elements, Fisher-Yates shuffle)
+#define array_shuffle(a)					\
+	do {							\
+		for (size_t i = array_lasti(a); i > 0; i--) {	\
+			array_swap(a, i, rand() % (i + 1));	\
+		}						\
+	} while (0)
 
-static void *__array_copy(void *arr, size_t elem_size)
+static void __array_swap(void *arr, size_t elem_size, unsigned char *buf, size_t i, size_t j)
 {
-	if (!arr) {
-		return NULL;
-	}
-	__arr *new_arr = (__arr *)malloc(sizeof(*new_arr) + elem_size * array_limit(arr));
-	memcpy(new_arr, __arrhead(arr), sizeof(*new_arr) + elem_size * array_len(arr));
-	return new_arr + 1;
+	size_t n = elem_size;
+	unsigned char *a = arr;
+	memcpy(buf,       &a[n * i], n);
+	memcpy(&a[n * i], &a[n * j], n);
+	memcpy(&a[n * j], buf,       n);
 }
 
 static void __array_free(void **arrp)
@@ -89,15 +153,15 @@ static void __array_free(void **arrp)
 	}
 }
 
-static void __array_resize(void **arrp, size_t elem_size, size_t limit)
+static void __array_resize(void **arrp, size_t elem_size, size_t capacity)
 {
-	if (limit == 0) {
+	if (capacity == 0) {
 		__array_free(arrp);
 		return;
 	}
-	size_t new_size = sizeof(__arr) + (limit * elem_size);
+	size_t new_size = sizeof(__arr) + (capacity * elem_size);
 #if ARRAY_SAFETY_CHECKS
-	assert(((limit * elem_size) / elem_size == limit) && new_size > sizeof(__arr));
+	assert(((capacity * elem_size) / elem_size == capacity) && new_size > sizeof(__arr));
 #endif
 	__arr *head;
 	if (!(*arrp)) {
@@ -109,34 +173,43 @@ static void __array_resize(void **arrp, size_t elem_size, size_t limit)
 #endif
 	} else {
 		head = realloc(__arrhead(*arrp), new_size);
-		if (head->len > limit) {
-			head->len = limit;
+		if (head->len > capacity) {
+			head->len = capacity;
 		}
 	}
-	head->limit = limit;
+	head->capacity = capacity;
 	*arrp = head + 1;
 }
 
-// increase limit by atleast n elements
+static void *__array_copy(void *arr, size_t elem_size)
+{
+	void *new_arr = NULL;
+	__array_resize(&new_arr, elem_size, array_capacity(arr));
+	__arrhead(new_arr)->len = array_len(arr);
+	memcpy(new_arr, arr, elem_size * array_len(arr));
+	return new_arr;
+}
+
 static void __array_grow(void **arrp, size_t elem_size, size_t n)
 {
 	if (n == 0) {
 		return;
 	}
-	size_t limit = array_limit(*arrp);
-	size_t new_limit = n < limit ? 2 * limit : limit + n;
-	if (new_limit < ARRAY_INITIAL_SIZE) {
-		new_limit = ARRAY_INITIAL_SIZE;
+	size_t capacity = array_capacity(*arrp);
+	size_t new_capacity = n < capacity ? 2 * capacity : capacity + n;
+	if (new_capacity < ARRAY_INITIAL_SIZE) {
+		new_capacity = ARRAY_INITIAL_SIZE;
 	}
-	__array_resize(arrp, elem_size, new_limit);
+	__array_resize(arrp, elem_size, new_capacity);
 }
 
 static void __array_reserve(void **arrp, size_t elem_size, size_t n)
 {
+	// TODO implement this as a macro?
 	if (n == 0) {
 		return;
 	}
-	size_t rem = array_limit(*arrp) - array_len(*arrp);
+	size_t rem = array_capacity(*arrp) - array_len(*arrp);
 	if (n > rem) {
 		__array_grow(arrp, elem_size, n - rem);
 	}
@@ -144,9 +217,9 @@ static void __array_reserve(void **arrp, size_t elem_size, size_t n)
 
 static void __array_make_valid(void **arrp, size_t elem_size, size_t i)
 {
-	size_t limit = array_limit(*arrp);
-	if (i >= limit) {
-		__array_grow(arrp, elem_size, i - limit + 1);
+	size_t capacity = array_capacity(*arrp);
+	if (i >= capacity) {
+		__array_grow(arrp, elem_size, i - capacity + 1);
 	}
 	if (i >= array_len(*arrp)) {
 		__arrhead(*arrp)->len = i + 1;
@@ -163,8 +236,8 @@ static void *__array_addn(void **arrp, size_t elem_size, size_t n)
 	__arr *head = __arrhead(*arrp);
 	size_t old_len = head->len;
 	head->len += n;
-	if (head->len > head->limit) {
-		__array_grow(arrp, elem_size, head->len - head->limit);
+	if (head->len > head->capacity) {
+		__array_grow(arrp, elem_size, head->len - head->capacity);
 	}
 	return (char *)(*arrp) + (old_len * elem_size);
 }
